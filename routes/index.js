24734373a9ad82ee22ab7f041ecff57cb0f518d0
xiafanju <chenysc@outlook.com>
2018-05-17 21:50:14 +0800

    add mongodb link

--- routes/index.js
+++ routes/index.js
@@ -34,4 +34,27 @@ router.put('/user/:username',function (req, res) {
     res.send('done')
 });
 
+
+router.get('/u/:user',function (req,res) {
+
+});
+router.post('/post',function (req,res) {
+
+});
+router.get('/reg',function (req,res) {
+
+});
+router.post('/reg',function (req,res) {
+
+});
+router.get('/login',function (req,res) {
+
+});
+router.post('/login',function (req,res) {
+
+});
+router.get('/logout',function (req,res) {
+
+});
+
 module.exports = router;
